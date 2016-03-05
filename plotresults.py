6442cc9552989292376403b04c3b6f7dcdbf8ab6
rohit21122012 <rohit21122012@gmail.com>
2016-03-05 12:53:06 +0530

    corr in plot

--- plotresults.py
+++ plotresults.py
@@ -4,12 +4,12 @@ import re
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_cm(cm, targets, title="Confusion Matrix", cmap=plt.cm.Blues, norm=True):
+def plot_cm(cm, acc, targets, title="Confusion Matrix", cmap=plt.cm.Blues, norm=True):
     if(norm):
         cm = cm.astype(float)/cm.sum(axis=1)[:, np.newaxis]
     fig = plt.figure()
     plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
+    plt.title(title + ' Acc:' + str(acc))
     plt.colorbar()
 
     tick_marks = np.arange(len(targets))
@@ -30,4 +30,5 @@ for f in glob.glob('result*.pkl'):
    res = joblib.load(f)
    clf_name = re.search('(?<=result)\w+(?=.pkl)', f).group(0)
    print clf_name
-   plot_cm(res['tot_cm'], res['result']['class_wise_accuracy'].keys(), title='Confusion Matrix - '+clf_name)
+   plot_cm(res['tot_cm'], res['result']['overall_accuracy'], res['result']['class_wise_accuracy'].keys(), title='Confusion Matrix - '+clf_name)
+
