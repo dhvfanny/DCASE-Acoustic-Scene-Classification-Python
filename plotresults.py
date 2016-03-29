18ead8f886cd3cb954f6e09b4c08037c74f40f1a
rohit21122012 <rohit21122012@gmail.com>
2016-03-29 21:00:19 +0530

    modified plot result

--- plotresults.py
+++ plotresults.py
@@ -2,14 +2,18 @@ from sklearn.externals import joblib
 import glob 
 import re
 import matplotlib.pyplot as plt
+import matplotlib.axes as axes
 import numpy as np
 
 def plot_cm(cm, acc, targets, title="Confusion Matrix", cmap=plt.cm.Blues, norm=True):
     if(norm):
         cm = cm.astype(float)/cm.sum(axis=1)[:, np.newaxis]
+    
     fig = plt.figure()
+    ax = plt.gca()
+   
     plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title + ' Acc: ' + str(round(acc,2)))
+    plt.title('Clf: ' + title + ' Acc: ' + str(round(acc,4)*100))
     plt.colorbar()
 
     tick_marks = np.arange(len(targets))
@@ -21,7 +25,7 @@ def plot_cm(cm, acc, targets, title="Confusion Matrix", cmap=plt.cm.Blues, norm=
 
     # plt.show()
     
-    fig.savefig(title + '.png')
+    fig.savefig(title + '.png',  bbox_inches='tight')
     
     plt.close()
     
@@ -30,4 +34,5 @@ for f in glob.glob('result*.pkl'):
    res = joblib.load(f)
    clf_name = re.search('(?<=result)\w+(?=.pkl)', f).group(0)
    print clf_name
-   plot_cm(res['tot_cm'], res['result']['overall_accuracy'], res['result']['class_wise_accuracy'].keys(), title='Clf: '+clf_name)
+   plot_cm(res['tot_cm'], res['result']['overall_accuracy'], res['result']['class_wise_accuracy'].keys(), title=clf_name)
+
